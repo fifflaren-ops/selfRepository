e63d94667884ec4d563ef9bf0fbcc142a6d0e4ad
fifflaren-ops <2233703203@qq.com>
2020-04-17 09:34:31 +0800

    TestApplication modify

--- src/test/java/cn/jj/test/ClinicApplicationTest.java
+++ src/test/java/cn/jj/test/ClinicApplicationTest.java
@@ -1,15 +1,12 @@
 package cn.jj.test;
 
-import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import cn.jj.pojo.Member;
 import cn.jj.service.DepartmentService;
-import cn.jj.service.MemberService;
 
 @SpringBootTest
 public class ClinicApplicationTest {
